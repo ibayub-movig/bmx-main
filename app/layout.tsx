89565525ab5d7e2f2240e36e307a757f5143aef6
ibayub-movig <ibrahim@getmovig.com>
2025-01-20 18:34:23 -0500

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { GoogleAnalytics } from '@next/third-parties/google'
 import Script from 'next/script'
+import {Analytics} from '@vercel/analytics/next'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -133,6 +134,7 @@ export default function RootLayout({
         )}
         {children}
         {GA_ID && <GoogleAnalytics gaId={GA_ID} />}
+        <Analytics/>
       </body>
     </html>
   )
